d37a64ff50deb5d632839e43d80a1aeaf254e8fe
bestrolla <angelmanzano01092003@gmail.com>
2024-10-29 21:09:16 -0400

    on branch main

--- proyec4.0/header.php
+++ proyec4.0/header.php
@@ -20,7 +20,7 @@
                     <li><a href="index.php">Inicio</a></li>
                     <li><a href="Cursos.php">Cursos</a></li>
                     <li><a href="eventos.php">Eventos</a></li>
-                    <li><a href="post.php">instagram</a></li>
+                    <li><a href="instagram/feed.php">instagram</a></li>
                     <li><a href="calendario.php">Calendario</a></li>
                     <li><a href="agenda.php">agenda</a></li>
                     <li><a href="inscrip.php">Inscribete</a></li>
