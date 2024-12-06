54002ed2d76952ee7f9c933c00bbccb5db30e080
bestrolla <angelmanzano01092003@gmail.com>
2024-12-06 11:02:27 -0400

    on branch main

---
+++ insta/visual/html/feed.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>complejo |instagram</title>
+     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="ig.css">
+    <link rel="shortcut icon" href="LOGOOFICIAL.png" type="image/x-icon">
+    <?php include('../head.php');?>
+    <link rel="stylesheet" href="../styles.css">
+    
+</head>
+<header>
+            <div class="container">       
+            <div class="logo">
+                <img src="img/logo.jpg" alt="UNIMET"  height="75px " width="75px"  >
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="../index.php">Inicio</a></li>
+                    <!-- <li><a href="Cursos.php">Cursos</a></li> -->
+                    <li><a href="../calendario_admin.php">Eventos</a></li>
+                    <li><a href="instagram/feed.php">instagram</a></li>
+                    <li><a href="../calendario.php">Calendario</a></li>
+                    <!-- <li><a href="agenda.php">agenda</a></li> -->
+                    <li><a href="../inscrip.php">Inscribete</a></li>
+                </ul>
+            </nav>
+        </div>
+        </header>
+<style>
+    hr {
+      border-top-width: 2px solid #0e0000;
+      border-radius: 5px;
+      width: 50%;
+      margin-top: 50px auto;
+    }
+
+    .contenedor-titulos {
+  display: flex;
+  justify-content:center ; /* Distribuye los elementos de forma equitativa */
+}
+
+.titulo {
+  /* Estilos para cada título */
+  text-align: center;
+ 
+}
+
+.boton-seguir {
+ 
+  color: rgb(10, 5, 5);
+  padding: 12px 27px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  background-color: #92a0af2d; /* Cambia el color al pasar el ratón */
+  box-shadow: 0px 5px 10px rgba(0,0,0,0.2);
+  border-radius: 10px; /* Aquí se establece el radio del borde */
+  cursor: pointer;
+}
+  </style>
+<body>
+    <a href="https://www.instagram.com/alcaldiasaa/">
+    <img src="instagramlogo.png" alt="icono" width="150px">
+</a>
+
+    <div class="instagram-profile">
+        <img src="logoig.jpg" class="imagen-Logo" alt="Icono de perfil">
+        <h1 class="stat">alcaldiasaa</h1> 
+        <div class="actions">
+            <button class="boton-seguir">Seguir</button>
+        </div>
+            <div class="actions">
+            <button class="boton-seguir">Mensaje</button>
+        </div>
+    </div>
+    <div class="instagram-profile">
+        <br>
+        <p > <br>
+            <strong> Alcaldia del Municipio Los Salias</strong> <br>
+            Local service <br>
+            Cuenta Oficial<br>
+            Alcalde Jose Fernandez "Josy" @josyfernandez<br>
+            Vivir para servir</p>
+        <div class="stats">
+            </div>
+        
+        <br>
+    </div>
+        <div class="instagram-profile">
+            <div class="stat">
+                <p>5,676</p>
+                <p>Posts</p>
+            </div>
+            <div class="stat">
+                <p>42.4k</p>
+                <p>Followers</p>
+            </div>
+            <div class="stat">
+                <p>1,671</p>
+                <p>Following</p>
+            </div>
+        </div>
+    </div>
+    <div class="instagram-profile">
+        <div class="highlights">
+           
+            <div class="highlight-grid">
+                <div class="highlight-item">
+                    <img src="reloj.png" alt="Destacado 1">
+                </div>
+                <div class="highlight-item">
+                    <img src="empresa.png" alt="Destacado 2">
+                </div>
+                <div class="highlight-item">
+                    <img src="proteccion.png" alt="Destacado 3">
+                </div>
+                <div class="highlight-item">
+                    <img src="salud.png" alt="Destacado 3">
+                </div>
+                <div class="highlight-item">
+                    <img src="fundacion.png" alt="Destacado 3">
+                </div>
+                <div class="highlight-item">
+                    <img src="seguridad.png" alt="Destacado 3">
+                </div>
+                
+                </div>
+                
+                
+        </div>
+    </div>
+
+    <hr> <br>
+
+    <div class="contenedor-titulos">
+            <img class="imagen" src="cuadricula.png" alt="Mi imagen" width="20px">
+            <div class="titulo">
+              post
+            </div>
+        </div>
+    <div class="contenedor-titulos">
+            <img class="imagen" src="reels.png" alt="Mi imagen" width="20px">
+            <div class="titulo">
+              Reels
+            </div>
+    </div>
+    <div class="contenedor-titulos">
+        <img class="imagen" src="etiquetas.png" alt="Mi imagen" width="20px">
+        <div class="titulo">
+          Etiquetas
+        </div>
+</div>
+    
+          
+    
+    <section class="feed">
+        <div class="post">
+            <img src="img1.jpg" alt="Imagen de la publicación 1">
+            <div class="post-info">
+                <div class="user">
+                    <img src="logoig.jpg" alt="Avatar del usuario">
+                    <span>alcaldiasaa</span>
+                </div>
+                <div class="actions">
+                    <button class="like">Like</button>
+                    <button class="comment"> Comentar</button>
+                </div>
+                <p class="caption">La alcaldia del municipio Los Salias, a traves de la direccion de la Juventud los invita a la participacion de la TRIVIA SAA ¡Los esperamos bachilleres! <br>
+                    #trivia #municipio #bachiller</p>
+                
+                <div class="comments">
+                    <p><strong>@carlota.parraa</strong> <br>
+                         ¡Que foto tan increible!</p>
+                    <p><strong>@jospulido11</strong> <br>
+                         Gracias por este post</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="post">
+            <img src="prevencion2.jpg" alt="Imagen de la publicación 1">
+            <div class="post-info">
+                <div class="user">
+                    <img src="logoig.jpg" alt="Avatar del usuario">
+                    <span>alcaldiasaa</span>
+                </div>
+                <div class="actions">
+                    <button class="like">Like</button>
+                    <button class="comment"> Comentar</button>
+                </div>
+                <p class="caption">La alcaldia del municipio Los Salias, a traves de la direccion de la Juventud los invita a la participacion de la TRIVIA SAA ¡Los esperamos bachilleres! <br>
+                    #trivia #municipio #bachiller</p>
+                    <time datetime="2023-11-22">22 de noviembre de 2023</time>
+                
+                <div class="comments">
+                    <p><strong>@jaissielb</strong> <br>
+                         que foto tan genial</p>
+                    <p><strong>@kyomichabella25</strong> <br>
+                         felicidadeeeeees</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="post">
+            <img src="jornada2.jpg" alt="Imagen de la publicación 1">
+            <div class="post-info">
+                <div class="user">
+                    <img src="logoig.jpg" alt="Avatar del usuario">
+                    <span>alcaldiasaa</span>
+                </div>
+                    <div class="actions">
+                        <button class="like">Like</button>
+                        <button class="comment"> Comentar</button>
+                    </div>
+                
+                <p class="caption">La alcaldia del municipio Los Salias, a traves de la direccion de la Juventud los invita a la participacion de la TRIVIA SAA ¡Los esperamos bachilleres! <br>
+                    #trivia #municipio #bachiller 
+                </p>
+               
+                <div class="comments">
+                    <p><strong>@luisamorays03</strong> <br>
+                            Los mejores foreveer</p>
+                    <p><strong>@keller_mejias08</strong> <br>
+                        Estoy de acuerdo</p>
+                </div>
+            </div>
+        </div>
+        </section>
+        <?php include('../footer.php');?>
+
+</body>
+</html>
+
+
+
